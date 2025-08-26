3bbbbda1be03c8b8bf50325d7b355ef812c83714
Algot <algot.gronvall@hotmail.com>
2025-08-26 10:54:36 +0200

    boss

---
+++ Moment 1/www/public/answers.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <?php
+            $ans1 = $_POST['qOne'];
+
+            echo "<p>Fråga 1: $ans1</p>";
+        ?>
+    </main>
+</body>
+</html>
