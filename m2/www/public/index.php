e9a723399191dc7f748b2f5d137de5723fda3e68
Algot <algot.gronvall@hotmail.com>
2025-08-28 13:10:56 +0200

    added uppgift 1-3

--- m2/www/public/index.php
+++ m2/www/public/index.php
@@ -14,7 +14,20 @@
    <main>
       <?php
          echo strip_tags("<p>Hej <i><b>världen!</b></i></p>", "<p><b>");
-      ?>
+
+         for ($i=0; $i <= 5; $i+=0.1) { 
+            echo "<p>Nummer: " . $i . "</p>";
+         }
+
+         $page["head"] = "<h1>Välkommen</h1>";
+         $page["body"] = "<p>Detta är innehållet på min sida</p>";
+         $page["footer"] = "<hr><p>Min sidfoot</p>";
+
+         foreach($page as $section) {
+            echo $section;
+         }
+
+       ?>
    </main>
 </body>
 
